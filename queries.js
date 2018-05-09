285e857b650691093fe64986707a6088c946293f
migser <mserranocaldeiro@salesforce.com>
2018-05-09 16:02:40 +0200

    accediendo a la bbdd test

--- queries.js
+++ queries.js
@@ -10,12 +10,25 @@ var connectionString = process.env.DATABASE_URL;
 var db = pgp(connectionString);
 console.log('Conectado a la BBDD');
 // add query functions
-/*
+
+function getCustomer(req, res, next) {
+    var customer = parseInt(req.params.id);
+    db.one('select * from test where B = $1', customer)
+        .then(function (data) {
+            res.status(200)
+                .json({
+                    status: 'success',
+                    data: data,
+                    message: 'Retrieved ONE Customer'
+                });
+        })
+        .catch(function (err) {
+            return next(err);
+        });
+}
+
+
 module.exports = {
-    getAllPuppies: getAllPuppies,
-    getSinglePuppy: getSinglePuppy,
-    createPuppy: createPuppy,
-    updatePuppy: updatePuppy,
-    removePuppy: removePuppy
+    getCustomer: getCustomer
 };
-*/
+
--- routes/mangoRouter.js
+++ routes/mangoRouter.js
@@ -1,29 +1,19 @@
 const express = require('express');
 var unirest = require('unirest');
-//var token = '';
-//var mensaje = require('../mensaje')('https://miguel-iot.my.salesforce.com/services/data/v41.0/sobjects/');
-//var auth = require('../auth')('admin@miguel.iot', 'sfdc1234HJtedhTlK779SHBORSLHynRw', '3MVG9g9rbsTkKnAWdtjzTDiZAFT4z.PuJSnwGz9z0uACd_zN11ucyRmF9f.0fAx2qkJNu.MpIV4b4IIMqp1eS',
-//    '8769286430156645141',
-//    (response) => {
-//        console.log('Conectado Demo Miguel!');
-//        token = response.body.access_token;
-//    }
-//);
-//var evento = 'Tracker_Event__e';
+
 
 var db = require('../queries');
 
-const eventRouter = express.Router();
+const mangoRouter = express.Router();
 
-//auth.auth();
-
-eventRouter.route('/compra')
+mangoRouter.route('/compra/:id')
 .post((req, res, next) => {
             console.log('Test OK');
-            res.statusCode = 200;
+            db.getCustomer;
+            /*res.statusCode = 200;
             res.setHeader('Content-Type', 'application/json');
             var respuesta = { texto: 'Test OK' };
-            res.json(respuesta);
+            res.json(respuesta);*/
        
     
 });
