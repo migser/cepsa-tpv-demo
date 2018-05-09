667f8914507e59e0ed33b8e9fc83c09d2fb3fe41
migser <mserranocaldeiro@salesforce.com>
2018-05-09 16:26:49 +0200

    añadidos paramteros json

--- routes/mangoRouter.js
+++ routes/mangoRouter.js
@@ -22,12 +22,14 @@ mangoRouter.route('/compra')
     .post((req, res, next) => {
             console.log('Test OK2');
             console.log('Parametro: id --> ' + req.param('id'));
+            console.log('JSON: '+req.body);
             //db.getCustomer(req, res, next;
                 res.statusCode = 200;
                 res.setHeader('Content-Type', 'application/json');
                 var respuesta = {
                     texto: 'Test OK',
-                    id: req.param('id')
+                    id: req.param('id'),
+                    request: req.body
                 };
                 res.json(respuesta);
 
