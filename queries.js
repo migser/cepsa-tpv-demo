59c80fbeaf111d3599cc3dd2ee0bd4d0f023f0ba
Miguel Serrano <mserranocaldeiro@mserrano-ltm3.internal.salesforce.com>
2019-06-17 21:47:42 +0200

    funcion v11

--- queries.js
+++ queries.js
@@ -298,7 +298,7 @@ function getPromo(req, res, next) {
     } = req.body.ticket;
 
     // db.one('select * from actualiza2(\'x\',\'y\',33)', [ticketid, combustible, importe])
-    db.one('select * from actualiza2(\'x\',\'y\',33)')
+    db.one('select * from public.actualiza(\'x\',\'y\',33)')
         .then((data) => {
             res.status(200)
                 .json({
